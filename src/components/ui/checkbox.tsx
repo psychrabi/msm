9193ffc96cafa384bd62072f9d4883047dda59d9
Rabi Shrestha <psychrabi@gmail.com>
2026-08-25 22:15:41 +0545

    refactor(viewer): use shadcn checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,6 +1,20 @@
-import type { InputHTMLAttributes } from 'react';
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { Check } from 'lucide-react';
+import type { ComponentPropsWithoutRef } from 'react';
 import { cn } from '../../lib/utils';
 
-export function Checkbox({ className, ...props }: InputHTMLAttributes<HTMLInputElement>) {
-  return <input type="checkbox" className={cn('shadcn-checkbox', className)} {...props} />;
+export function Checkbox({ className, ...props }: ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>) {
+  return (
+    <CheckboxPrimitive.Root
+      className={cn(
+        'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+        className,
+      )}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
+        <Check className="h-4 w-4" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
 }
